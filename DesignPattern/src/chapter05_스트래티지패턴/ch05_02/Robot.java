55fb660ba44ebe2bb4194787af87bb686fa3c519
yonghwanKim <dragonbead95@naver.com>
2021-04-06 17:49:09 +0900

    strategy pattern

---
+++ DesignPattern/src/chapter05_스트래티지패턴/ch05_02/Robot.java
@@ -0,0 +1,18 @@
+package chapter05_스트래티지패턴.ch05_02;
+
+public abstract class Robot {
+	private String name;
+
+	public Robot(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public abstract void attack();
+	public abstract void move();
+}
---
+++ DesignPattern/src/chapter05_스트래티지패턴/ch05_03/strategy/moving/FlyingStrategy.java
@@ -0,0 +1,11 @@
+package chapter05_스트래티지패턴.ch05_03.strategy.moving;
+
+public class FlyingStrategy implements MovingStrategy{
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		System.out.println("I can fly.");
+	}
+	
+}
