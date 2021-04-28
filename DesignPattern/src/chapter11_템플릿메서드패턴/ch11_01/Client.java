bafc4ac595b5343f14ae584e56955dc88ae0c336
yonghwanKim <dragonbead95@naver.com>
2021-04-29 00:32:42 +0900

    template method pattern

---
+++ DesignPattern/src/chapter11_템플릿메서드패턴/ch11_01/Client.java
@@ -0,0 +1,12 @@
+package chapter11_템플릿메서드패턴.ch11_01;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Door door = new Door();
+		HyundaiMotor hyundaiMotor = new HyundaiMotor(door);
+		hyundaiMotor.move(Direction.UP);
+	}
+
+}
---
+++ DesignPattern/src/chapter11_템플릿메서드패턴/ch11_04/HyundaiMotor.java
@@ -0,0 +1,20 @@
+package chapter11_템플릿메서드패턴.ch11_04;
+
+
+
+public class HyundaiMotor extends Motor{
+	
+	
+	public HyundaiMotor(Door door) {
+		super(door);
+	}
+
+	@Override
+	protected void moveMotor(Direction direction) {
+		// TODO Auto-generated method stub
+		// HyundaiMotor를 구동시킴
+	}
+	
+	
+	
+}
